65ba96827c1ed68442f3daee79402f0cf2069cad
Arch <107460395+644731871zzz@users.noreply.github.com>
2024-12-05 17:59:35 +0800

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -18,6 +20,10 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship=Ship(self)
+        self.bullets=pygame.sprite.Group()
+        self.aliens=pygame.sprite.Group()
+
+        self._create_fleet()
 
     
     def run_game(self):
@@ -25,6 +31,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
     
@@ -46,6 +54,8 @@ class AlienInvasion:
             self.ship.moving_left=True
         elif event.key==pygame.K_q:
             sys.exit()
+        elif event.key==pygame.K_SPACE:
+            self._fire_bullet()
         
 
     def _check_keyup_events(self,event):
@@ -55,12 +65,74 @@ class AlienInvasion:
         elif event.key==pygame.K_LEFT:
             self.ship.moving_left=False
 
-                
+    def _fire_bullet(self):
+        """创建一颗子弹并加入编组"""
+        if len(self.bullets)<self.settings.bullets_allowed:  
+            new_bullet=Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除已经消失的子弹"""
+        #更新子弹的位置
+        self.bullets.update()
+
+        #删除已经消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <=0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
+
+    def _update_aliens(self):
+        """更新外星舰队中外星人的位置"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _create_fleet(self):
+        """创建一个外星人舰队"""
+        #创建一个外星人,再不断添加,直到没有空间添加外星人为止
+        #外星人的间距为外星人的宽度
+        alien=Alien(self)
+        alien_width,alien_height=alien.rect.size
+
+        current_x,current_y=alien_width,alien_height
+        while current_y <(self.settings.screen_height-3*alien_height):
+            while current_x<(self.settings.screen_width-2*alien_width):
+                self._create_alien(current_x,current_y)
+                current_x+=2*alien_width
+
+            #添加第一行外星人后重置x并且递增y
+            current_x=alien_width
+            current_y+=2*alien_height
+
+    def _create_alien(self,x_position,y_position):
+        """创建一个外星人并将其放在当前行中"""
+        new_alien=Alien(self)
+        new_alien.x=x_position
+        new_alien.rect.x=x_position
+        new_alien.rect.y=y_position
+        self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """在有外星人到达边缘时采取相应措施"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """将整个外星人舰队向下移动,并改变他们的方向"""
+        for alien in self.aliens.sprites():
+            alien.rect.y+=self.settings.fleet_drop_speed
+        self.settings.fleet_direction*=-1
     
     def _update_screen(self):
         """更新屏幕上的图像"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
+
         #将画面显示到屏幕上
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -15,3 +15,10 @@ class Settings:
         self.bullet_width=3
         self.bullet_height=15
         self.bullet_color=(60,60,160)
+        self.bullets_allowed=3
+
+        #外星人设置
+        self.alien_speed=1.0
+        self.fleet_drop_speed=10
+        #fleet_direction 为1向右移动 -1为向左移动
+        self.fleet_direction=1
